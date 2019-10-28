8494de9aa7db2b284b26a254c343b991058e615c
Shane Vitarana <shanev@gmail.com>
2019-10-28 17:00:00 -0400

    Updated ModuleName

--- incubator/wasm/internal/types/keys.go
+++ incubator/wasm/internal/types/keys.go
@@ -6,7 +6,7 @@ import (
 
 const (
 	// ModuleName is the name of the contract module
-	ModuleName = "contract"
+	ModuleName = "wasm"
 
 	// StoreKey is the string store representation
 	StoreKey = ModuleName
