c7ac80175a6f05acbf391359d8c2aca720f6a2e1
梁平 <18786721@qq.com>
2020-03-26 21:42:38 +0800

    add 'publish faucet key' and 'initial faucet keys'.

--- incubator/faucet/internal/types/errors.go
+++ incubator/faucet/internal/types/errors.go
@@ -7,4 +7,6 @@ import (
 var (
 	// ErrWithdrawTooOften withdraw too often
 	ErrWithdrawTooOften = sdkerrors.Register(ModuleName, 100, "Each address can withdraw only once")
+	ErrFaucetKeyEmpty   = sdkerrors.Register(ModuleName, 101, "Armor should Not be empty.")
+	ErrFaucetKeyExisted = sdkerrors.Register(ModuleName, 102, "Faucet key existed")
 )
--- incubator/faucet/internal/types/msgs.go
+++ incubator/faucet/internal/types/msgs.go
@@ -49,3 +49,41 @@ func (msg MsgMint) GetSignBytes() []byte {
 func (msg MsgMint) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Sender}
 }
+
+// MsgMint defines a mint message
+type MsgFaucetKey struct {
+	Sender sdk.AccAddress
+	Armor  string
+}
+
+// NewMsgFaucetKey is a constructor function for MsgFaucetKey
+func NewMsgFaucetKey(sender sdk.AccAddress, armor string) MsgFaucetKey {
+	return MsgFaucetKey{Sender: sender, Armor: armor}
+}
+
+// Route should return the name of the module
+func (msg MsgFaucetKey) Route() string { return RouterKey }
+
+// Type should return the action
+func (msg MsgFaucetKey) Type() string { return "faucet-key" }
+
+// ValidateBasic runs stateless checks on the message
+func (msg MsgFaucetKey) ValidateBasic() error {
+	if msg.Sender.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.Sender.String())
+	}
+	if len(msg.Armor) == 0 {
+		return ErrFaucetKeyEmpty
+	}
+	return nil
+}
+
+// GetSignBytes encodes the message for signing
+func (msg MsgFaucetKey) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(msg))
+}
+
+// GetSigners defines whose signature is required
+func (msg MsgFaucetKey) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Sender}
+}
