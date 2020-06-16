c67759bd79c5c224cc1ba754b8d49dec838f593a
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2020-06-16 16:04:37 +0900

    Fix query supply response garbled (#55)
    Co-authored-by: unknown <33382781+YuKimura45z@users.noreply.github.com>

--- incubator/nft/keeper/querier.go
+++ incubator/nft/keeper/querier.go
@@ -1,8 +1,8 @@
 package keeper
 
 import (
-	"encoding/binary"
 	"fmt"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -56,8 +56,7 @@ func querySupply(ctx sdk.Context, path []string, req abci.RequestQuery, k Keeper
 		return nil, sdkerrors.Wrap(types.ErrUnknownCollection, fmt.Sprintf("unknown denom %s", params.Denom))
 	}
 
-	bz := make([]byte, 8)
-	binary.LittleEndian.PutUint64(bz, uint64(collection.Supply()))
+	bz := []byte(strconv.Itoa(collection.Supply()))
 	return bz, nil
 }
 
